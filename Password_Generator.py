9e67063d85ab0c79f6907c11ed831c885f55b075
vhrs6 <115562280+vhrs6@users.noreply.github.com>
2023-10-22 02:15:37 +0530

    Add files via upload

---
+++ Password_Generator.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+
+letters = string.ascii_lowercase
+special_char = string.punctuation
+numbers = string.digits
+
+
+def pwd_generator(min_length, special_characters=True):
+    characters = letters + numbers
+
+    if special_characters:
+        characters += special_char
+    pwd = ""
+    meets_criteria = False
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if any(x in numbers for x in pwd):
+            meets_criteria = True
+    return pwd
+
+
+print(pwd_generator(6))
